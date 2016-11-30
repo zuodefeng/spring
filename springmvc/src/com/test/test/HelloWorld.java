3eab2c64630f9967cc5dbb2b4b4af590c1ae0dbe
zuodefeng <zuodefeng@msymobile.com>
2016-11-30 09:57:59 +0800

    hell world 提交

---
+++ springmvc/src/com/test/test/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.test.test;
+
+/**
+ * Created by admin on 2016/11/30.
+ */
+public class HelloWorld {
+
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void hello() {
+        System.out.print("hell " + name);
+    }
+}
